26fb04f48c5a2fa75f4765de960b0817bca68510
Guilherme Muniz <gui.munizalemao@gmail.com>
2025-06-05 10:37:32 -0300

    feat: add function loadUser

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,19 +1,29 @@
 import { BsSearch } from "react-icons/bs";
+import { useState } from "react";
 
-const Search = () => {
+type SearchProps = {
+  loadUser: (username: string) => Promise<void>;
+};
+
+const Search = ({ loadUser }: SearchProps) => {
+  const [username, setUsername] = useState("");
   return (
     <div className=" bg-blue-900 flex flex-col items-center justify-center gap-4 p-4">
       <h2 className="">Busque por usuário</h2>
       <p className="">Conheça seus melhores repositórios</p>
       <div className="flex items-center gap-2 bg-white p-2 rounded-lg">
-        <BsSearch className="text-gray-500" />
         <input
           type="text"
           placeholder="Digite seu nome de usuário"
           className=""
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
         />
-        <button className="px-4 py-2 rouded rouded text-black">
-          Pesquisar
+        <button
+          className="px-4 py-2 rouded rouded text-black"
+          onClick={() => loadUser(username)}
+        >
+          <BsSearch className="inline-block mr-2" />
         </button>
       </div>
     </div>
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,33 @@
 import Search from "../components/Search";
+import { useState } from "react";
+import { type UserProps } from "../types/user";
 
 const Home = () => {
+  const [user, setUser] = useState<UserProps | null>(null);
+
+  const loadUser = async (username: string) => {
+    const res = await fetch(`https://api.github.com/users/${username}`);
+
+    const data = await res.json();
+
+    console.log(data);
+
+    const { avatar_url, login, location, followers, following } = data;
+
+    const userData: UserProps = {
+      avatar_url,
+      login,
+      location,
+      followers,
+      following,
+    };
+
+    setUser(userData);
+  };
+
   return (
     <div>
-      <Search />
+      <Search loadUser={loadUser} />
     </div>
   );
 };
