84be6ed387a25c3b79c4f339bfd9d25443d66bda
Guilherme Muniz <gui.munizalemao@gmail.com>
2025-06-06 11:18:29 -0300

    feat: add component error

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -4,14 +4,25 @@ import { type UserProps } from "../types/user";
 // components
 import User from "../components/User";
 import Search from "../components/Search";
+import Error from "../components/Error";
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState<boolean | null>(null);
 
   const loadUser = async (username: string) => {
+    setError(false);
+    setUser(null);
+
     const res = await fetch(`https://api.github.com/users/${username}`);
 
     const data = await res.json();
+
+    if (res.status === 404) {
+      setError(true);
+      setUser(null);
+      return;
+    }
 
     console.log(data);
 
@@ -33,6 +44,7 @@ const Home = () => {
     <div>
       <Search loadUser={loadUser} />
       {user && <User {...user} />}
+      {error && <Error />}
     </div>
   );
 };
